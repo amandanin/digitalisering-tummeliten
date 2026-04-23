3eba75e83e4db83d9840b89f83b774ced79143e2
amandanin <amandaelizaolsson@gmail.com>
2026-04-23 12:58:00 +0200

    Började koda på läsvy
    Laddade upp exempelbilder (tbd senare)

---
+++ docs/assets/js/script.js
@@ -0,0 +1,18 @@
+
+//  zooma in på bilder i läsvy
+const img = document.querySelector('.zoom-img');
+
+document.querySelectorAll('.zoom-img').forEach(img => {
+  img.addEventListener('mousemove', function(e) {
+    const rect = this.getBoundingClientRect();
+    const x = (e.clientX - rect.left) / rect.width * 100;
+    const y = (e.clientY - rect.top) / rect.height * 100;
+
+    this.style.transformOrigin = `${x}% ${y}%`;
+    this.style.transform = "scale(2)";
+  });
+
+  img.addEventListener('mouseleave', function() {
+    this.style.transform = "scale(1)";
+  });
+});
